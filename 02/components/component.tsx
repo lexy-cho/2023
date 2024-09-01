37d214b700227cbb4c4caef3f3bca2cd6facda6c
Minkyoung Cho <lexycho00@gmail.com>
2024-09-01 20:14:45 +0900

    Delete 02 directory

--- 02/components/component.tsx
+++
@@ -1,5 +0,0 @@
-import { NativeStackScreenProps } from '@react-navigation/native-stack';
-import { ParamListBase } from '@react-navigation/routers';
-
-export type MainScreenProps = NativeStackScreenProps<ParamListBase>;
-
--- 02/components/index.tsx
+++
@@ -1,6 +0,0 @@
-import Clock from "./Clock";
-import PopupModal from "./PopupModal";
-import ScrollToTopButton from "./ScrollToTopButton";
-import TimerTop from "./TimerTop";
-
-export {Clock, PopupModal, ScrollToTopButton, TimerTop};
